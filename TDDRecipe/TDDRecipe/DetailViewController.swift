499e86db6e344d8ce3d21ae9f2fc78baed0eb79d
torpedo87 <bestofhand@naver.com>
2017-09-18 17:43:08 +0900

    add detailViewController tests

---
+++ TDDRecipe/TDDRecipe/DetailViewController.swift
@@ -0,0 +1,112 @@
+//
+//  DetailViewController.swift
+//  TDDRecipe
+//
+//  Created by junwoo on 2017. 9. 15..
+//  Copyright © 2017년 junwoo. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DetailViewController: UIViewController {
+  let titleLabel: UILabel = {
+    let label = UILabel()
+    return label
+  }()
+  let locationLabel: UILabel = {
+    let label = UILabel()
+    return label
+  }()
+  let dateLabel: UILabel = {
+    let label = UILabel()
+    return label
+  }()
+  let descriptionLabel: UILabel = {
+    let label = UILabel()
+    return label
+  }()
+  let mapView: MKMapView = {
+    var view = MKMapView()
+    return view
+  }()
+  
+  var itemInfo: (ItemManager, Int)?
+  let dateFormatter: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "MM/dd/yyyy"
+    return dateFormatter
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.addSubview(mapView)
+    self.view.addSubview(titleLabel)
+    self.view.addSubview(locationLabel)
+    self.view.addSubview(dateLabel)
+    self.view.addSubview(descriptionLabel)
+    
+    view.setNeedsUpdateConstraints()
+  }
+  
+  override func updateViewConstraints() {
+    mapView.snp.makeConstraints { make in
+      make.height.equalTo(100)
+      make.width.equalTo(200)
+      make.center.equalTo(self.view)
+    }
+    titleLabel.snp.makeConstraints { make in
+      make.height.equalTo(50)
+      make.width.equalTo(self.view)
+      make.top.equalTo(mapView.snp.bottom)
+    }
+    
+    dateLabel.snp.makeConstraints { make in
+      make.height.equalTo(50)
+      make.width.equalTo(self.view)
+      make.top.equalTo(titleLabel.snp.bottom)
+    }
+    
+    locationLabel.snp.makeConstraints { make in
+      make.height.equalTo(50)
+      make.width.equalTo(self.view)
+      make.top.equalTo(dateLabel.snp.bottom)
+    }
+    
+    descriptionLabel.snp.makeConstraints { make in
+      make.height.equalTo(50)
+      make.width.equalTo(self.view)
+      make.top.equalTo(locationLabel.snp.bottom)
+    }
+    super.updateViewConstraints()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    guard let itemInfo = itemInfo else { return }
+    let item = itemInfo.0.item(at: itemInfo.1)
+    
+    titleLabel.text = item.title
+    locationLabel.text = item.location?.name
+    descriptionLabel.text = item.itemDescription
+    
+    if let timestamp = item.timestamp {
+      let date = Date(timeIntervalSince1970: timestamp)
+      dateLabel.text = dateFormatter.string(from: date)
+    }
+    
+    if let coordinate = item.location?.coordinate {
+      let region = MKCoordinateRegionMakeWithDistance(coordinate,
+                                                      100, 100)
+      mapView.region = region
+    }
+    
+  }
+  
+  func checkItem() {
+    if let itemInfo = itemInfo {
+      itemInfo.0.checkItem(at: itemInfo.1)
+    }
+  }
+}
---
+++ TDDRecipe/TDDRecipeTests/DetailViewControllerTests.swift
@@ -0,0 +1,104 @@
+//
+//  DetailViewControllerTests.swift
+//  TDDRecipeTests
+//
+//  Created by junwoo on 2017. 9. 15..
+//  Copyright © 2017년 junwoo. All rights reserved.
+//
+
+import XCTest
+@testable import TDDRecipe
+import CoreLocation
+
+class DetailViewControllerTests: XCTestCase {
+  
+  var sut: DetailViewController!
+  
+  override func setUp() {
+    super.setUp()
+    sut = DetailViewController()
+    
+    //viewdidload 호출
+    _ = sut.view
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  //titlelabel
+  func test_HasTitleLabel() {
+    
+    XCTAssertNotNil(sut.titleLabel)
+  }
+  
+  //locationlabel
+  func test_HasLocationLabel() {
+    
+    XCTAssertNotNil(sut.locationLabel)
+  }
+  
+  //datelabel
+  func test_HasDateLabel() {
+    
+    XCTAssertNotNil(sut.dateLabel)
+  }
+  
+  //descriptionlabel
+  func test_HasDescriptionLabel() {
+    
+    XCTAssertNotNil(sut.descriptionLabel)
+  }
+  
+  //mapview
+  func test_HasMapView() {
+    XCTAssertNotNil(sut.mapView)
+  }
+  
+  //label.text, mapview.region
+  func test_SettingItemInfo_SetsTextsToLabels() {
+    let coordinate = CLLocationCoordinate2DMake(51.2277, 6.7735)
+    
+    let location = Location(name: "Foo", coordinate: coordinate)
+    let item = ToDoItem(title: "Bar",
+                        itemDescription: "Baz",
+                        timestamp: 1456150025,
+                        location: location)
+    
+    let itemManager = ItemManager()
+    itemManager.add(item)
+    
+    sut.itemInfo = (itemManager, 0)
+    
+    //frame 값을 줘야 테스트 통과하는데 뭐지...???
+    sut.mapView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+    
+    //viewwillappear 호출
+    sut.beginAppearanceTransition(true, animated: true)
+    sut.endAppearanceTransition()
+    XCTAssertEqual(sut.titleLabel.text, "Bar")
+    XCTAssertEqual(sut.dateLabel.text, "02/22/2016")
+    XCTAssertEqual(sut.locationLabel.text, "Foo")
+    XCTAssertEqual(sut.descriptionLabel.text, "Baz")
+    XCTAssertEqual(sut.mapView.centerCoordinate.latitude,
+                               coordinate.latitude,
+                               accuracy: 0.001)
+    XCTAssertEqual(sut.mapView.centerCoordinate.longitude,
+                               coordinate.longitude,
+                               accuracy: 0.001)
+  }
+  
+  //check
+  func test_CheckItem_ChecksItemInItemManager() {
+    let itemManager = ItemManager()
+    itemManager.add(ToDoItem(title: "Foo"))
+    
+    sut.itemInfo = (itemManager, 0)
+    sut.checkItem()
+    
+    XCTAssertEqual(itemManager.toDoCount, 0)
+    XCTAssertEqual(itemManager.doneCount, 1)
+  }
+  
+}
